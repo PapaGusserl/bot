596a2c03918e877b88f8f4ba1cff37df62f819ef
vlad <akhtyamov.vlad@outlook.com>
2018-01-09 17:16:20 +0300

    (master): add bot_api.ex

--- lib/bot_api.ex
+++ lib/bot_api.ex
@@ -5,6 +5,7 @@ defmodule Bot.API do
     |> <> URI.encode(method)
     |> HTTPoison.post(request(opts), [], [timeout: timeout()])
     |> read_response
+    |> parse(method)
   end
 
   defp url() do
@@ -21,10 +22,37 @@ defmodule Bot.API do
   end
 
   defp read_response(%HTTPoison.Response{body: term, headers: list, request_url: term, status_code: 200}) do
-    #
+    %{result: result} =
+      term
+      |> Poison.decode!(keys: :atoms)
+    result
   end
 
    defp read_response(%HTTPoison.Response{body: term, headers: list, request_url: term, status_code: 404}) do
     #
-  end
-  
+   end
+
+   def parse(result, method) do
+     case method do
+       "getUpdates" -> 
+         parse_value(:updates, result)
+       "getMe" ->
+         parse_value(User, result)
+       _ -> 
+         parse_value(Message, result)
+     end
+   end
+
+   def parse_value(:updates, result) do 
+     Enum.map(result, &(parse_value(Update, &1)))
+   end
+
+   def parse_value(agent, result) do
+     struct(agent, Enum.map(result, &(parse_value(&1))))
+   end
+
+   def parse_value({key, value}) do
+     {key, Enum.map(value, &(parse_value(&1)))}
+   end
+
+end  
