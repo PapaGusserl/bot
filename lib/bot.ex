399c44a6a4653890f125c415efc2b62d4ff405aa
vlad <akhtyamov.vlad@outlook.com>
2018-01-09 23:31:59 +0300

    (master): fix bot.ex and bot_api.ex

--- lib/bot.ex
+++ lib/bot.ex
@@ -1,8 +1,8 @@
 defmodule Bot.Worker do
 
-  def getUpdates(%{update_id: offset, timeout: timeout) do
-    cmd("getUpdates", %{offset: offset, timeout: timeout}
-    %{update_id: update_id, timeout: timeout}
+  def getUpdates(%{update_id: offset, timeout: timeout}) do
+    cmd("getUpdates", %{offset: offset, timeout: timeout})
+    %{update_id: offset, timeout: timeout}
   end
 
   def cmd(method, args) do
@@ -19,6 +19,8 @@ defmodule Bot.Worker do
     process_job(job)
   end
 
-  def process_job(job)
+  def process_job(job) do
+    :noreply
+  end
     
 end
--- lib/bot_api.ex
+++ lib/bot_api.ex
@@ -1,36 +1,39 @@
 defmodule Bot.API do
 
   def post(method, opts) do
-    url()
-    |> <> URI.encode(method)
+    url(method)
     |> HTTPoison.post(request(opts), [], [timeout: timeout()])
     |> read_response
-    |> parse(method)
+   #|> parse(method)
   end
 
-  defp url() do
-    #
+  defp url(method) do
+    "https://api.telegram.org/bot#{token()}/#{to_string(method)}"
+  end
+
+  defp token do
+    "537321635:AAED7FVU2uECKmfkrea3XSL1app7kDe7Js0"
   end
 
   defp timeout() do
     #
   end
 
-  defp request(opts) do
+  def request(opts) do
     opts = opts
-           |> Map.update(:reply_markup, nil, &(Poison.encode!(&1)))
-           |> Enum.filter_map(fn {_, v} -> v end, fn {k, v} -> {k, to_string(v)} end)
+           |> Map.update(:reply_markup, nil, &(Poison.encode!(&1)))  ## add to map 'opts' 'reply_markup: nil' and encode to json all values in opts
+           |> Enum.filter(fn {_, v} -> v end)
+           |> Enum.map(fn {k, v} -> {k, to_string(v)} end)
       {:form, opts}
   end
 
-  defp read_response(%HTTPoison.Response{body: term, headers: list, request_url: term, status_code: 200}) do
-    %{result: result} =
-      term
+  defp read_response({:ok, %HTTPoison.Response{body: term, status_code: 200}}) do
+    %{result: result} = term
       |> Poison.decode!(keys: :atoms)
     result
   end
 
-   defp read_response(%HTTPoison.Response{body: term, headers: list, request_url: term, status_code: 404}) do
+  defp read_response(%HTTPoison.Response{body: term, status_code: 404}) do
     #
    end
 
