6746e47f6f7a9159b26061d15e1f0367719ecf3d
vlad <akhtyamov.vlad@outlook.com>
2018-01-21 21:07:06 +0300

    (cava): add someshit

---
+++ lib/imp_shit.ex
@@ -0,0 +1,10 @@
+defmodule ImpShit do
+  def token(bot) do
+    case bot do
+       :cava_nch -> "540187835:AAEIS_caMtm1sd04fR1z65uvWH6gg2gGvLM"
+      
+      :akhtyamov -> "537321635:AAED7FVU2uECKmfkrea3XSL1app7kDe7Js0"
+    end
+  end
+
+end
